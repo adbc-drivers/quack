af68bab2d364d0a3d9b3d23e959a50d63e7117d5
David Li <li.davidm96@gmail.com>
2026-05-21 15:33:13 +0900

    chore: fix build

--- ci/scripts/generate_license.sh
+++ ci/scripts/generate_license.sh
@@ -18,4 +18,4 @@ set -euo pipefail
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 repo_root="$(cd "${script_dir}/../.." && pwd)"
 
-cat "${repo_root}/LICENSE.txt"
+cat "${repo_root}/license.tpl"
